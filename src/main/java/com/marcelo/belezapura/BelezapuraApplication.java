79caff8b8c6efc0b68cffe19d23d427154dcb4ab
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-09-23 17:53:45 -0300

    chore: commit inicial

---
+++ src/main/java/com/marcelo/belezapura/BelezapuraApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.belezapura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelezapuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelezapuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo/belezapura/BelezapuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.belezapura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelezapuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
